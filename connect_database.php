c944b99b1bafc90e1a8e0a273181fb132fa6cfaf
haonx <qtvhao@gmail.com>
2017-07-19 19:03:23 +0700

    rename to same meaning

--- connect_database.php
+++ connect_database.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div>connecting to database</div>';
 $database_connector = new mysqli();
-$database_server_hostname = 'localhost';
-$database_connector->connect($database_server_hostname);//database server is 'localhost' (XAMPP default database)
+$db_host = 'localhost';
+$database_connector->connect($db_host);//database server is 'localhost' (XAMPP default database)
 return '<div>This string will be returned to function require_once()</div>';

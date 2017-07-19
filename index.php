17ad09e3e2e2c6b029eadd6d6e296c7ed9518d80
haonx <qtvhao@gmail.com>
2017-07-19 19:30:43 +0700

    run SQL for create Database

--- index.php
+++ index.php
@@ -10,3 +10,13 @@ echo '<pre>';
 // https://www.w3schools.com/php/php_mysql_create.asp
 /** @var mysqli $connection */
 print_r($connection);
+// Create database
+$database_name = 'mydb';
+$sql = "CREATE DATABASE $database_name";
+$mysqli_result = $connection->query($sql);
+if ($mysqli_result === TRUE) {
+    echo "<div>Database created successfully</div>";
+} else {
+    $error_message = $connection->error;
+    echo "<div>Error creating database: <span style='color:red'>$error_message</span></div>";
+}

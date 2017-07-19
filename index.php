0a9b711a007e9c37a959e11593a72628633e897a
haonx <qtvhao@gmail.com>
2017-07-19 20:46:40 +0700

    run sql for create table 'tasks'

--- index.php
+++ index.php
@@ -13,3 +13,11 @@ echo '<pre>';
 $database_name = 'mydb';
 $sql = "CREATE DATABASE $database_name";
 $connection->query($sql);
+$sql_create_table_tasks = 'CREATE TABLE `tasks` ( `id` INT NOT NULL AUTO_INCREMENT , `title` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;';
+echo '<div style="color:Blue">INFO: this SQL string was auto-generated from <a href="//localhost/phpmyadmin" target="_blank">phpMyAdmin</a></div>';
+$mysqli_result = $connection->query($sql_create_table_tasks);
+if ($mysqli_result) {
+    echo "<div style='color:Green'>Table 'tasks' created successfully</div>";
+} else {
+    echo "<div style='color:Red'>Error creating table: $connection->error</div>";
+}

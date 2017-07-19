90b623057978f906f76a552764a64e465eb4212c
haonx <qtvhao@gmail.com>
2017-07-19 19:25:08 +0700

    login to mysql database

--- connect_database.php
+++ connect_database.php
@@ -3,4 +3,7 @@ echo '<div>connecting to database</div>';
 $database_connector = new mysqli();
 $db_host = 'localhost';
 $database_connector->connect($db_host);//database server is 'localhost' (XAMPP default database)
+$mysql_username_login = 'root';
+$mysql_password = '';
+$database_connector->change_user($mysql_username_login, $mysql_password, null);
 return $database_connector;

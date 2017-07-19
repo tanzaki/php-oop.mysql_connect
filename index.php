2ba5b08a8b48b2564b0fe6d6c1ddf787579e5fbc
haonx <qtvhao@gmail.com>
2017-07-19 21:08:03 +0700

    clean up

--- index.php
+++ index.php
@@ -16,9 +16,4 @@ $connection->query($sql);
 $connection->select_db($database_name);
 $sql_create_table_tasks = 'CREATE TABLE `tasks` ( `id` INT NOT NULL AUTO_INCREMENT , `title` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;';
 echo '<div style="color:Blue">INFO: this SQL string was auto-generated from <a href="//localhost/phpmyadmin" target="_blank">phpMyAdmin</a></div>';
-$mysqli_result = $connection->query($sql_create_table_tasks);
-if ($mysqli_result) {
-    echo "<div style='color:Green'>Table 'tasks' created successfully</div>";
-} else {
-    echo "<div style='color:Red'>Error creating table: $connection->error</div>";
-}
+$connection->query($sql_create_table_tasks);

832d7125d5bfadc6371d9e18d5b465d495985708
haonx <qtvhao@gmail.com>
2017-07-19 19:34:38 +0700

    clean up

--- connect_database.php
+++ connect_database.php
@@ -1,5 +1,4 @@
 <?php
-echo '<div>connecting to database</div>';
 $database_connector = new mysqli();
 $db_host = 'localhost';
 $database_connector->connect($db_host);//database server is 'localhost' (XAMPP default database)
--- index.php
+++ index.php
@@ -9,14 +9,7 @@ $connection = require_once 'connect_database.php';
 echo '<pre>';
 // https://www.w3schools.com/php/php_mysql_create.asp
 /** @var mysqli $connection */
-print_r($connection);
 // Create database
 $database_name = 'mydb';
 $sql = "CREATE DATABASE $database_name";
-$mysqli_result = $connection->query($sql);
-if ($mysqli_result === TRUE) {
-    echo "<div>Database created successfully</div>";
-} else {
-    $error_message = $connection->error;
-    echo "<div>Error creating database: <span style='color:red'>$error_message</span></div>";
-}
+$connection->query($sql);

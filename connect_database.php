a3ea9b11f9b18a684496b80437c8fdcec6fbb261
haonx <qtvhao@gmail.com>
2017-07-19 19:10:05 +0700

    return $database_connector to index.php

--- connect_database.php
+++ connect_database.php
@@ -3,4 +3,4 @@ echo '<div>connecting to database</div>';
 $database_connector = new mysqli();
 $db_host = 'localhost';
 $database_connector->connect($db_host);//database server is 'localhost' (XAMPP default database)
-return '<div>This string will be returned to function require_once()</div>';
+return $database_connector;
